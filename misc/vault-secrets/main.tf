791fd0a3450abead11e86e6c1e6e6ffb9102bb7a
kishor <kishorebsc12@gmail.com>
2025-08-31 12:52:12 +0530

    added

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b86"
+    key = "vault-secrets/state"
+    region = "us-east-1"
+  }
+}
+
+provider "vault" {
+  address = "http://vault.kommanuthala.store:8200"
+  token= var.vault_token
+}
+
+variable "vault_token" {}
+
+resource "vault_generic_secret" "ssh" {
+  path = "infra/ssh"
+
+  data_json = <<EOT
+  {
+  "username" : "ec2-user",
+  "paasword" : "DevOps321"
+  }
+  EOT
+}
