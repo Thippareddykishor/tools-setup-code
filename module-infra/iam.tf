c50177d7c5cdb6c55d7872b12283d126ec91fe75
kishor <kishorebsc12@gmail.com>
2025-09-14 21:16:20 +0530

    added

--- module-infra/iam.tf
+++ module-infra/iam.tf
@@ -2,7 +2,7 @@ resource "aws_iam_role" "main" {
   name= "${var.name}-role"
 
   assume_role_policy = jsonencode({
-    version = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
         {
             Action ="sts:AssumeRole"
