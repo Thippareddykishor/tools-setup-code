b370ac4a1c1d23d85c19adc5449cee90df87abe9
kishor <kishorebsc12@gmail.com>
2025-11-07 15:26:10 +0530

    added

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -124,6 +124,7 @@ resource "vault_generic_secret" "roboshop-dev-shipping" {
   "APP_GIT_URL"  :   "https://github.com/roboshop-devops-project-v3/shipping",
   "DB_USER"      :   "root",
   "DB_PASS"      :    "RoboShop@1"
+  "NEW_RELIC_LICENSE_KEY" : ""
   }
   EOT
 }
